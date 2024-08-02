7122bf66c6894a9d299b4ba9eef9dc20c0e34d7e
Tomas Dabasinskas <tomas@dabasinskas.net>
2024-08-02 11:22:44 +0300

    Return spec for entities as JSON
    Took 3 hours 15 minutes

--- backstage/data_source_entities.go
+++ backstage/data_source_entities.go
@@ -2,7 +2,9 @@ package backstage
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"github.com/hashicorp/terraform-plugin-framework-jsontypes/jsontypes"
 	"net/http"
 
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
@@ -35,6 +37,7 @@ type entityDataSourceModel struct {
 
 type entityModel struct {
 	ApiVersion types.String          `tfsdk:"api_version"`
+	Spec       jsontypes.Normalized  `tfsdk:"spec"`
 	Kind       types.String          `tfsdk:"kind"`
 	Metadata   *entityMetadataModel  `tfsdk:"metadata"`
 	Relations  []entityRelationModel `tfsdk:"relations"`
@@ -124,6 +127,7 @@ func (d *entityDataSource) Schema(_ context.Context, _ datasource.SchemaRequest,
 			"entities": schema.ListNestedAttribute{Computed: true, Description: descriptionEntitySpec, NestedObject: schema.NestedAttributeObject{
 				Attributes: map[string]schema.Attribute{
 					"api_version": schema.StringAttribute{Computed: true, Description: descriptionEntityApiVersion},
+					"spec":        schema.StringAttribute{Computed: true, Description: descriptionEntitySpec, CustomType: jsontypes.NormalizedType{}},
 					"kind":        schema.StringAttribute{Computed: true, Description: descriptionEntityKind},
 					"metadata": schema.SingleNestedAttribute{Computed: true, Description: descriptionEntityMetadata, Attributes: map[string]schema.Attribute{
 						"uid":         schema.StringAttribute{Computed: true, Description: descriptionEntityMetadataUID},
@@ -201,9 +205,19 @@ func (d *entityDataSource) Read(ctx context.Context, req datasource.ReadRequest,
 	state.ID = types.StringValue(fmt.Sprint(state.Filters))
 
 	for _, e := range entities {
+		v, err := json.Marshal(e.Spec)
+		if err != nil {
+			resp.Diagnostics.AddError(
+				"Error parsing Backstage entity specs",
+				fmt.Sprintf("Could not parse Specs for Backstage entity %v: %s", e.Metadata.Name, err.Error()),
+			)
+			continue
+		}
+
 		entity := entityModel{
 			ApiVersion: types.StringValue(e.ApiVersion),
 			Kind:       types.StringValue(e.Kind),
+			Spec:       jsontypes.NewNormalizedValue(string(v)),
 		}
 
 		for _, i := range e.Relations {
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/h2non/gock v1.2.0
 	github.com/hashicorp/terraform-plugin-docs v0.18.0
 	github.com/hashicorp/terraform-plugin-framework v1.7.0
+	github.com/hashicorp/terraform-plugin-framework-jsontypes v0.1.0
 	github.com/hashicorp/terraform-plugin-framework-validators v0.12.0
 	github.com/hashicorp/terraform-plugin-go v0.22.1
 	github.com/hashicorp/terraform-plugin-log v0.9.0
@@ -21,6 +22,7 @@ require (
 	github.com/Masterminds/sprig/v3 v3.2.3 // indirect
 	github.com/ProtonMail/go-crypto v1.1.0-alpha.0 // indirect
 	github.com/agext/levenshtein v1.2.2 // indirect
+	github.com/apparentlymart/go-textseg/v12 v12.0.0 // indirect
 	github.com/apparentlymart/go-textseg/v15 v15.0.0 // indirect
 	github.com/armon/go-radix v1.0.0 // indirect
 	github.com/bgentry/speakeasy v0.1.0 // indirect
