887b75fd67a4a54f3a09cebef384e71ad56b190b
Vibhor Kainth <vibhor.kainth@1komma5grad.com>
2024-11-22 11:54:49 +0100

    feat: fallback for groups datasource

--- backstage/data_source_group_test.go
+++ backstage/data_source_group_test.go
@@ -31,3 +31,48 @@ data "backstage_group" "test" {
   name = "team-a"
 }
 `
+
+func TestAccDataSourceGroup_WithFallback(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: `
+					data "backstage_group" "test" {
+						name = "team_does_not_compute_a9ab8"
+						fallback = {
+							api_version = "backstage.io/v1alpha1"
+							name = "fallback_team"
+							kind = "Group"
+							metadata = {
+								description = "Fallback Team A"
+								annotations = {
+									"backstage.io/source-location" = "url:fallback.com"
+								}
+							}
+							spec = {
+								parent = "backstage"
+							}
+							relations = [
+								{
+									type = "childOf"
+									target_ref = "backstage"
+								}
+							]
+						}
+					}	
+				`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("data.backstage_group.test", "api_version", "backstage.io/v1alpha1"),
+					resource.TestCheckResourceAttr("data.backstage_group.test", "kind", "Group"),
+					resource.TestCheckResourceAttr("data.backstage_group.test", "metadata.description", "Fallback Team A"),
+					resource.TestCheckResourceAttr("data.backstage_group.test", "metadata.annotations.backstage.io/source-location",
+						"url:fallback.com"),
+					resource.TestCheckResourceAttr("data.backstage_group.test", "relations.0.type", "childOf"),
+					resource.TestCheckResourceAttr("data.backstage_group.test", "spec.parent", "backstage"),
+					resource.TestCheckResourceAttr("data.backstage_group.test", "name", "fallback_team"),
+				),
+			},
+		},
+	})
+}
