9ea1027dd2ebd7937f87a76c35c3b2b5603ed162
Vibhor Kainth <vibhor.kainth@1komma5grad.com>
2024-11-22 14:12:48 +0100

    feat: fallback for user datasource

--- backstage/data_source_user.go
+++ backstage/data_source_user.go
@@ -39,6 +39,7 @@ type userDataSourceModel struct {
 	Metadata   *entityMetadataModel  `tfsdk:"metadata"`
 	Relations  []entityRelationModel `tfsdk:"relations"`
 	Spec       *userSpecModel        `tfsdk:"spec"`
+	Fallback   *userFallbackModel    `tfsdk:"fallback"`
 }
 
 type userSpecModel struct {
@@ -52,12 +53,24 @@ type userSpecProfileModel struct {
 	Picture     types.String `tfsdk:"picture"`
 }
 
+type userFallbackModel struct {
+	ID         types.String          `tfsdk:"id"`
+	Name       types.String          `tfsdk:"name"`
+	Namespace  types.String          `tfsdk:"namespace"`
+	ApiVersion types.String          `tfsdk:"api_version"`
+	Kind       types.String          `tfsdk:"kind"`
+	Metadata   *entityMetadataModel  `tfsdk:"metadata"`
+	Relations  []entityRelationModel `tfsdk:"relations"`
+	Spec       *userSpecModel        `tfsdk:"spec"`
+}
+
 const (
 	descriptionUserSpecProfile            = "Profile information about the user, mainly for display purposes."
 	descriptionUserSpecProfileDisplayName = "A simple display name to present to users."
 	descriptionUserSpecProfileEmail       = "Email where this user can be reached."
 	descriptionUserSpecProfilePicture     = "A URL of an image that represents this user."
 	descriptionUserSpecMemberOf           = "The list of groups that the user is a direct member of (i.e., no transitive memberships are listed here)."
+	descriptionUserFallback               = "A complete replica of the `User` as it would exist in backstage. Set this to provide a fallback in case the Backstage instance is not functioning, is down, or is unrealiable."
 )
 
 // Metadata returns the data source type name.
@@ -127,6 +140,64 @@ func (d *userDataSource) Schema(_ context.Context, _ datasource.SchemaRequest, r
 					"picture":      schema.StringAttribute{Computed: true, Description: descriptionUserSpecProfilePicture},
 				}},
 			}},
+			"fallback": schema.SingleNestedAttribute{Optional: true, Description: descriptionUserFallback, Attributes: map[string]schema.Attribute{
+				"id": schema.StringAttribute{Optional: true, Description: descriptionEntityMetadataUID},
+				"name": schema.StringAttribute{Required: true, Description: descriptionEntityMetadataName, Validators: []validator.String{
+					stringvalidator.LengthBetween(1, 63),
+					stringvalidator.RegexMatches(
+						regexp.MustCompile(patternEntityName),
+						"must follow Backstage format restrictions",
+					),
+				}},
+				"namespace": schema.StringAttribute{Optional: true, Description: descriptionEntityMetadataNamespace, Validators: []validator.String{
+					stringvalidator.LengthBetween(1, 63),
+					stringvalidator.RegexMatches(
+						regexp.MustCompile(patternEntityName),
+						"must follow Backstage format restrictions",
+					),
+				}},
+				"api_version": schema.StringAttribute{Optional: true, Description: descriptionEntityApiVersion},
+				"kind":        schema.StringAttribute{Optional: true, Description: descriptionEntityKind},
+				"metadata": schema.SingleNestedAttribute{Optional: true, Description: descriptionEntityMetadata, Attributes: map[string]schema.Attribute{
+					"uid":         schema.StringAttribute{Optional: true, Description: descriptionEntityMetadataUID},
+					"etag":        schema.StringAttribute{Optional: true, Description: descriptionEntityMetadataEtag},
+					"name":        schema.StringAttribute{Optional: true, Description: descriptionEntityMetadataName},
+					"namespace":   schema.StringAttribute{Optional: true, Description: descriptionEntityMetadataNamespace},
+					"title":       schema.StringAttribute{Optional: true, Description: descriptionEntityMetadataTitle},
+					"description": schema.StringAttribute{Optional: true, Description: descriptionEntityMetadataDescription},
+					"labels":      schema.MapAttribute{Optional: true, Description: descriptionEntityMetadataLabels, ElementType: types.StringType},
+					"annotations": schema.MapAttribute{Optional: true, Description: descriptionEntityMetadataAnnotations, ElementType: types.StringType},
+					"tags":        schema.ListAttribute{Optional: true, Description: descriptionEntityMetadataTags, ElementType: types.StringType},
+					"links": schema.ListNestedAttribute{Optional: true, Description: descriptionEntityMetadataLinks, NestedObject: schema.NestedAttributeObject{
+						Attributes: map[string]schema.Attribute{
+							"url":   schema.StringAttribute{Optional: true, Description: descriptionEntityLinkURL},
+							"title": schema.StringAttribute{Optional: true, Description: descriptionEntityLinkTitle},
+							"icon":  schema.StringAttribute{Optional: true, Description: descriptionEntityLinkIco},
+							"type":  schema.StringAttribute{Optional: true, Description: descriptionEntityLinkType},
+						},
+					}},
+				}},
+				"relations": schema.ListNestedAttribute{Optional: true, Description: descriptionEntityRelations, NestedObject: schema.NestedAttributeObject{
+					Attributes: map[string]schema.Attribute{
+						"type":       schema.StringAttribute{Optional: true, Description: descriptionEntityRelationType},
+						"target_ref": schema.StringAttribute{Optional: true, Description: descriptionEntityRelationTargetRef},
+						"target": schema.SingleNestedAttribute{Optional: true, Description: descriptionEntityRelationTarget,
+							Attributes: map[string]schema.Attribute{
+								"name":      schema.StringAttribute{Optional: true, Description: descriptionEntityRelationTargetName},
+								"kind":      schema.StringAttribute{Optional: true, Description: descriptionEntityRelationTargetKind},
+								"namespace": schema.StringAttribute{Optional: true, Description: descriptionEntityRelationTargetNamespace},
+							}},
+					},
+				}},
+				"spec": schema.SingleNestedAttribute{Optional: true, Description: descriptionEntitySpec, Attributes: map[string]schema.Attribute{
+					"member_of": schema.ListAttribute{Optional: true, Description: descriptionUserSpecMemberOf, ElementType: types.StringType},
+					"profile": schema.SingleNestedAttribute{Optional: true, Description: descriptionUserSpecProfile, Attributes: map[string]schema.Attribute{
+						"display_name": schema.StringAttribute{Optional: true, Description: descriptionUserSpecProfileDisplayName},
+						"email":        schema.StringAttribute{Optional: true, Description: descriptionUserSpecProfileEmail},
+						"picture":      schema.StringAttribute{Optional: true, Description: descriptionUserSpecProfilePicture},
+					}},
+				}},
+			}},
 		},
 	}
 }
@@ -156,78 +227,104 @@ func (d *userDataSource) Read(ctx context.Context, req datasource.ReadRequest, r
 	tflog.Debug(ctx, fmt.Sprintf("Getting User kind %s/%s from Backstage API", state.Name.ValueString(), state.Namespace.ValueString()))
 	user, response, err := d.client.Catalog.Users.Get(ctx, state.Name.ValueString(), state.Namespace.ValueString())
 	if err != nil {
-		resp.Diagnostics.AddError(
-			"Error reading Backstage User kind",
-			fmt.Sprintf("Could not read Backstage User kind %s/%s: %s", state.Namespace.ValueString(), state.Name.ValueString(), err.Error()),
-		)
-		return
+		const shortErr = "Error reading Backstage User kind"
+		longErr := fmt.Sprintf("Could not read Backstage User kind %s/%s: %s", state.Namespace.ValueString(), state.Name.ValueString(), err.Error())
+		if state.Fallback == nil {
+			resp.Diagnostics.AddError(shortErr, longErr)
+			return
+		}
+		resp.Diagnostics.AddWarning(shortErr, longErr)
 	}
 
 	if response.StatusCode != http.StatusOK {
-		resp.Diagnostics.AddError(
-			"Error reading Backstage User kind",
-			fmt.Sprintf("Could not read Backstage User kind %s/%s: %s", state.Namespace.ValueString(), state.Name.ValueString(), response.Status),
-		)
-		return
+		const shortErr = "Error reading Backstage User kind"
+		longErr := fmt.Sprintf("Could not read Backstage User kind %s/%s: %s", state.Namespace.ValueString(), state.Name.ValueString(), response.Status)
+		if state.Fallback == nil {
+			resp.Diagnostics.AddError(shortErr, longErr)
+			return
+		}
+		resp.Diagnostics.AddWarning(shortErr, longErr)
 	}
 
-	state.ID = types.StringValue(user.Metadata.UID)
-	state.ApiVersion = types.StringValue(user.ApiVersion)
-	state.Kind = types.StringValue(user.Kind)
-
-	for _, i := range user.Relations {
-		state.Relations = append(state.Relations, entityRelationModel{
-			Type:      types.StringValue(i.Type),
-			TargetRef: types.StringValue(i.TargetRef),
-			Target: &entityRelationTargetModel{
-				Kind:      types.StringValue(i.Target.Kind),
-				Name:      types.StringValue(i.Target.Name),
-				Namespace: types.StringValue(i.Target.Namespace)},
-		})
+	if (err != nil || response.StatusCode != http.StatusOK) && state.Fallback != nil {
+		if state.Fallback.ID.IsNull() {
+			state.Fallback.ID = types.StringValue("123456789")
+		}
+		if state.Fallback.ApiVersion.IsNull() {
+			state.Fallback.ApiVersion = types.StringValue("backstage.io/v1alpha1")
+		}
+		if state.Fallback.Kind.IsNull() {
+			state.Fallback.Kind = types.StringValue(backstage.KindSystem)
+		}
+		state.ID = state.Fallback.ID
+		state.Name = state.Fallback.Name
+		state.Namespace = state.Fallback.Namespace
+		state.ApiVersion = state.Fallback.ApiVersion
+		state.Kind = state.Fallback.Kind
+		state.Metadata = state.Fallback.Metadata
+		state.Relations = state.Fallback.Relations
+		state.Spec = state.Fallback.Spec
 	}
 
-	state.Spec = &userSpecModel{
-		Profile: &userSpecProfileModel{
-			DisplayName: types.StringValue(user.Spec.Profile.DisplayName),
-			Email:       types.StringValue(user.Spec.Profile.Email),
-			Picture:     types.StringValue(user.Spec.Profile.Picture),
-		},
-	}
+	if err == nil && response.StatusCode == http.StatusOK {
+		state.ID = types.StringValue(user.Metadata.UID)
+		state.ApiVersion = types.StringValue(user.ApiVersion)
+		state.Kind = types.StringValue(user.Kind)
 
-	for _, i := range user.Spec.MemberOf {
-		state.Spec.MemberOf = append(state.Spec.MemberOf, types.StringValue(i))
-	}
+		for _, i := range user.Relations {
+			state.Relations = append(state.Relations, entityRelationModel{
+				Type:      types.StringValue(i.Type),
+				TargetRef: types.StringValue(i.TargetRef),
+				Target: &entityRelationTargetModel{
+					Kind:      types.StringValue(i.Target.Kind),
+					Name:      types.StringValue(i.Target.Name),
+					Namespace: types.StringValue(i.Target.Namespace)},
+			})
+		}
 
-	state.Metadata = &entityMetadataModel{
-		UID:         types.StringValue(user.Metadata.UID),
-		Etag:        types.StringValue(user.Metadata.Etag),
-		Name:        types.StringValue(user.Metadata.Name),
-		Namespace:   types.StringValue(user.Metadata.Namespace),
-		Title:       types.StringValue(user.Metadata.Title),
-		Description: types.StringValue(user.Metadata.Description),
-		Annotations: map[string]string{},
-		Labels:      map[string]string{},
-	}
+		state.Spec = &userSpecModel{
+			Profile: &userSpecProfileModel{
+				DisplayName: types.StringValue(user.Spec.Profile.DisplayName),
+				Email:       types.StringValue(user.Spec.Profile.Email),
+				Picture:     types.StringValue(user.Spec.Profile.Picture),
+			},
+		}
 
-	for k, v := range user.Metadata.Labels {
-		state.Metadata.Labels[k] = v
-	}
+		for _, i := range user.Spec.MemberOf {
+			state.Spec.MemberOf = append(state.Spec.MemberOf, types.StringValue(i))
+		}
 
-	for k, v := range user.Metadata.Annotations {
-		state.Metadata.Annotations[k] = v
-	}
+		state.Metadata = &entityMetadataModel{
+			UID:         types.StringValue(user.Metadata.UID),
+			Etag:        types.StringValue(user.Metadata.Etag),
+			Name:        types.StringValue(user.Metadata.Name),
+			Namespace:   types.StringValue(user.Metadata.Namespace),
+			Title:       types.StringValue(user.Metadata.Title),
+			Description: types.StringValue(user.Metadata.Description),
+			Annotations: map[string]string{},
+			Labels:      map[string]string{},
+		}
 
-	for _, v := range user.Metadata.Tags {
-		state.Metadata.Tags = append(state.Metadata.Tags, types.StringValue(v))
-	}
+		for k, v := range user.Metadata.Labels {
+			state.Metadata.Labels[k] = v
+		}
 
-	for _, v := range user.Metadata.Links {
-		state.Metadata.Links = append(state.Metadata.Links, entityLinkModel{
-			URL:   types.StringValue(v.URL),
-			Title: types.StringValue(v.Title),
-			Icon:  types.StringValue(v.Icon),
-			Type:  types.StringValue(v.Type),
-		})
+		for k, v := range user.Metadata.Annotations {
+			state.Metadata.Annotations[k] = v
+		}
+
+		for _, v := range user.Metadata.Tags {
+			state.Metadata.Tags = append(state.Metadata.Tags, types.StringValue(v))
+		}
+
+		for _, v := range user.Metadata.Links {
+			state.Metadata.Links = append(state.Metadata.Links, entityLinkModel{
+				URL:   types.StringValue(v.URL),
+				Title: types.StringValue(v.Title),
+				Icon:  types.StringValue(v.Icon),
+				Type:  types.StringValue(v.Type),
+			})
+		}
 	}
 
 	diags := resp.State.Set(ctx, state)
--- backstage/data_source_user_test.go
+++ backstage/data_source_user_test.go
@@ -1,6 +1,7 @@
 package backstage
 
 import (
+	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
@@ -32,3 +33,42 @@ data "backstage_user" "test" {
   name = "janelle.dawe"
 }
 `
+
+func TestAccDataSourceUser_WithFallback(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: `
+					data "backstage_user" "test" {
+						name = "user_not_found_blasted_a9ab8"
+						fallback = {
+							name = "fallback_user"
+						}
+					}
+				`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("data.backstage_user.test", "api_version", "backstage.io/v1alpha1"),
+					resource.TestCheckResourceAttr("data.backstage_user.test", "kind", "System"),
+					resource.TestCheckResourceAttr("data.backstage_user.test", "name", "fallback_user"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccDataSourceUser_WithoutFallback_Error(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: `
+					data "backstage_user" "test" {
+						name = "user_not_found_emoji_a9ab8"
+					}
+				`,
+				ExpectError: regexp.MustCompile(`default/user_not_found_emoji_a9ab8`),
+			},
+		},
+	})
+}
